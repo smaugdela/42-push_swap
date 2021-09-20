0a7d1cdd2292560907a869950ebd94ae1e3eb7ea
Simon MAGDELAINE <smagdela@student.42.fr>
2021-09-20 11:19:49 +0200

    Parsing and stacks initialization OK

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: smagdela <smagdela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 18:46:57 by smagdela          #+#    #+#             */
-/*   Updated: 2021/09/16 14:54:24 by smagdela         ###   ########.fr       */
+/*   Updated: 2021/09/20 10:47:30 by smagdela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 # include <stdint.h>
 
 /*   /!\ ATTENTION Supprimer cet include ATTENTION /!\   */
+
+typedef	struct s_stack
+{
+	size_t	len;
+	int		*list;
+}	t_stack;
 
 int	ft_error(int argc, char **argv);
 
--- sources/push_swap.c
+++ sources/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: smagdela <smagdela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 18:01:42 by smagdela          #+#    #+#             */
-/*   Updated: 2021/09/17 17:55:29 by smagdela         ###   ########.fr       */
+/*   Updated: 2021/09/20 11:18:56 by smagdela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,52 +21,47 @@ https://www.jesuisundev.com/comprendre-les-algorithmes-de-tri-en-7-minutes/
 
 #include "push_swap.h"
 
-static t_list	*ft_init(int argc, char **argv)
+static int	*ft_init(int argc, char **argv)
 {
-	int		i;
-	int		*value;
-	t_list	*list;
-	
-	i = argc;
-	list = NULL;
-	while (--i > 0)
-	{
-		value = (int*)malloc(sizeof(int));
-		if (value == NULL)
-		{
-			ft_lstclear(&list, &free);
-			free(value);
-			return (NULL);
-		}
-		*value = ft_atoi(argv[i]);
-		ft_lstadd_front(&list, ft_lstnew(value));
-		free(value);
-	}
+	int	i;
+	int	*list;
+
+	list = (int*)malloc((argc - 1) * sizeof(int));
+	if (list == NULL)
+		return (NULL);
+	i = 0;
+	while (++i < argc)
+		list[i - 1] = ft_atoi(argv[i]);
 	return (list);
 }
 
 int	main(int argc, char **argv)
 {
-	t_list	*stack_A;
-	t_list	*start;
-//	t_list	*stack_B;
+	t_stack	stack_A;
+	t_stack stack_B;
+	size_t	i;
 
 	if (ft_error(argc, argv))
 		return (42);
-	stack_A = ft_init(argc, argv);
-	if (stack_A == NULL || ft_lstsize(stack_A) != argc - 1)
+	stack_A.list = ft_init(argc, argv);
+	stack_B.list = (int*)malloc((argc - 1) * sizeof(int));
+	if (stack_A.list == NULL || stack_B.list == NULL)
 	{
-		write(2, "Init failed\n", 12);
+		free(stack_A.list);
+		free(stack_B.list);
 		return (42);
 	}
-	start = stack_A;
-	while (stack_A != NULL)
+	stack_A.len = argc - 1;
+	stack_B.len = 0;
+	i = 0;
+	while (i < stack_A.len)
 	{
-		write(1, "|", 1);
-		ft_putnbr_fd(*(int*)(stack_A->content), 1);
-		write(1, "|\n", 2);
-		stack_A = stack_A->next;
+		ft_putstr_fd("|", 1);
+		ft_putnbr_fd(stack_A.list[i], 1);
+		ft_putstr_fd("|\n", 1);
+		++i;
 	}
-	ft_lstclear(&start, &free);
+	free(stack_A.list);
+	free(stack_B.list);
 	return (0);
 }
